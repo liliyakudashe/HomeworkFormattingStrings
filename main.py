1971e63b6bd43a237d38b9a92985a54cf83e3fe6
Acer <oduvan4ik_888@mail.ru>
2024-01-08 19:16:58 +0300

    Домашнее задание по теме "Форматирование строк".

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+team1_num = 5
+result_str = "В команде Мастера кода участников: %d ! " % team1_num
+print(result_str)
+
+team2_num = 6
+result_str = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+print(result_str)
+
+score_2 = 42
+result_str = "Команда Волшебники данных решила задач: {} !".format(score_2)
+print(result_str)
+
+team2_time = 18015.2
+result_str = "Волшебники данных решили задачи за {} с !".format(team2_time)
+print(result_str)
+
+score_1 = 40
+result_str = f"Команды решили {score_1} и {score_2} задач."
+print(result_str)
+
+challenge_result = 'Победа команды Волшебники данных!'
+result_str = f"Результат битвы: {challenge_result}"
+print(result_str)
+
+tasks_total = 82
+time_avg = 350.4
+result_str = f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!"
+print(result_str)
+
 
